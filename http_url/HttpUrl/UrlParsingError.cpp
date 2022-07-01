0200bb92cb6444e11be1338393e38d022684dc2f
AlexandrProhorov <79852979+AlexandrProhorov@users.noreply.github.com>
2022-07-01 16:44:44 +0300

    Delete http_url directory

--- http_url/HttpUrl/UrlParsingError.cpp
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "UrlParsingError.h"
-
-UrlParsingError::UrlParsingError(const std::string& message)
-	: std::invalid_argument(message)
-{
-}
--- http_url/HttpUrl/UrlParsingError.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pch.h"
-#include <stdexcept>
-
-class UrlParsingError : public std::invalid_argument
-{
-public:
-	explicit UrlParsingError(const std::string& message);
-};
